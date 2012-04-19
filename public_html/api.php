b4d15b3862e3a64d219bf4024fbc97538cea0481
Pranav Gandhi <pranavrulz@gmail.com>
2012-04-19 23:51:24 +0200

    Switched order of file.txt and added a few more sanitizer things

--- public_html/api.php
+++ public_html/api.php
@@ -7,9 +7,9 @@ if(isset($_GET['falsify'])){
 } else {
   $file = file_get_contents("../file.txt");
   $array = explode("|", $file);
-  /* $array[0] is room, $array[1] is timestamp */
-  $room = $array[0];
-  $time = intval($array[1]);
+  /* $array[0] is timestamp, $array[1] is room */
+  $room = $array[1];
+  $time = intval($array[0]);
   $diff = time() - $time;
   $recent = "false";
   if($diff < 3600) $recent = "true";
--- public_html/sms.php
+++ public_html/sms.php
@@ -1,13 +1,12 @@
 <?php
 
-$time = time();
-
 if($_POST['Body'] != NULL){
-  $body = strip_tags($_POST['Body']);
+  $body = trim($_POST['Body']);
+  $body = strip_tags($body);
   $body = htmlentities($body);
   mysql_real_escape_string($body);
   if(strlen($body) < 3) die();
-  $towrite = $body.'|'.time();
+  $towrite = time().'|'.$body;
   $file = fopen("../file.txt", "w+");
   fwrite($file, $towrite);
 }
