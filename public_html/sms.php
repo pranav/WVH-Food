c00f3bf148daeef9cb285b22c902dd33181614ad
Pranav Gandhi <pranavrulz@gmail.com>
2012-04-19 23:30:52 +0200

    Rudimentary space checking

--- public_html/sms.php
+++ public_html/sms.php
@@ -6,7 +6,7 @@ if($_POST['Body'] != NULL){
   $body = strip_tags($_POST['Body']);
   $body = htmlentities($body);
   mysql_real_escape_string($body);
-		      
+  if(strlen($body) < 3) die();
   $towrite = $body.'|'.time();
   $file = fopen("../file.txt", "w+");
   fwrite($file, $towrite);
