91932745128d0bb44b1092d545dae05b6eac8612
Pranav Gandhi <pranavrulz@gmail.com>
2012-04-19 23:19:20 +0200

    Added an api call to force a NOPE

--- public_html/api.php
+++ public_html/api.php
@@ -1,15 +1,20 @@
 <?php
 
-$file = file_get_contents("../file.txt");
-$array = explode("|", $file);
-/* $array[0] is room, $array[1] is timestamp */
-$room = $array[0];
-$time = intval($array[1]);
-$diff = time() - $time;
-$recent = "false";
-if($diff < 3600) $recent = "true";
-header("Content-Type: application/json");
-echo "{\"room\":\"$room\", \"recent\":\"$recent\"}";
 
+
+if(isset($_GET['falsify'])){
+  fwrite(fopen("../file.txt", "w"), "0|0");
+} else {
+  $file = file_get_contents("../file.txt");
+  $array = explode("|", $file);
+  /* $array[0] is room, $array[1] is timestamp */
+  $room = $array[0];
+  $time = intval($array[1]);
+  $diff = time() - $time;
+  $recent = "false";
+  if($diff < 3600) $recent = "true";
+  header("Content-Type: application/json");
+  echo "{\"room\":\"$room\", \"recent\":\"$recent\"}";
+}
 
 ?>
