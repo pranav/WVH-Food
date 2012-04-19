5742b7a4abd549cb414e57f97daffc4fcb763cc0
Pranav Gandhi <pranavrulz@gmail.com>
2012-04-19 23:03:45 +0200

    Made a few sanitization changes

--- public_html/sms.php
+++ public_html/sms.php
@@ -6,8 +6,8 @@ if($_POST['Body'] != NULL){
   $body = htmlentities($_POST['Body']);
   mysql_real_escape_string($body);
 		      
-  $towrite = $_POST['Body'].'|'.time();
-  $file = fopen("../file.txt", "w");
+  $towrite = $body.'|'.time();
+  $file = fopen("../file.txt", "w+");
   fwrite($file, $towrite);
 }
 
