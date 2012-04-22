688a9871baca3171a519f472727e7318cd99a46f
Pranav Gandhi <pranavrulz@gmail.com>
2012-04-22 08:24:00 +0200

    Refactored code to use mysqli

--- lib.php
+++ lib.php
@@ -2,28 +2,47 @@
 
 require_once("config.php");
 
-function name_exists($from){
-  mysql_connect("localhost", DB_USER, DB_PASS);
-  mysql_select_db(DB_NAME);
-  $q = mysql_query("SELECT * from name WHERE from = '$from' LIMIT 1");
-  $num = mysql_num_rows($q);
-  if($num == 1) return true;
-  else return false;
-
-  
-} 
-
-
-function set_name($from, $name){
-  mysql_connect("localhost", DB_USER, DB_PASS);
-  mysql_select_db(DB_NAME);
-  if(name_exists($from)) {
-    echo "name_exists()";
-    mysql_query("UPDATE name SET name = '$name' WHERE from = '$from'");
-  }
+/* Connects to the database */
+function connect(){
+  $mysqli = new mysqli('localhost', DB_USER, DB_PASS, DB_NAME);
+  if(mysqli_connect_errno())
+    die(mysqli_connect_error());
   else
-    mysql_query("INSERT INTO name (name, from) VALUES ('$name', '$from')");
-  echo "INSERT_INTO()";
+    return $mysqli;
+}
+
+/* Queries the database */
+function query($sql){
+  $mysqli = connect();
+  $result = $mysqli->query($sql) or die($mysqli->error.__LINE__);
+  return $result;
+}
+
+/* Sanitizes the given input.  */
+function sanitize($text){
+  $body = trim($text);
+  $body = strip_tags($body);
+  $mysqli = connect();
+  $mysqli->real_escape_string($body);
+  $body = htmlentities($body);
+  
+  return $body;
+}
+
+/* Adds the user to the name table. If the user exists, it will update the entry. */
+function set_name($from, $name){
+  $result = query("SELECT * FROM name WHERE from = '$from' LIMIT 1");
+  if($result->num_rows > 0) {
+    query("UPDATE name SET name = '$name' WHERE from = '$from'");
+  } else {
+    query("INSERT INTO name (name, from) VALUES ('$name', '$from')");
+  }
+}
+
+/* Add a text to the table texts */
+function add_text($text, $from){
+  $time = time();
+  query("INSERT INTO texts (text, number, timestamp) VALUES ('$text', '$from', '$time')");
 }
 
 ?>
--- public_html/api.php
+++ public_html/api.php
@@ -1,40 +1,12 @@
 <?php
-require("../config.php");
-require("../lib.php");
+require_once("../lib.php");
 
-
-if(isset($_GET['falsify'])){
-  fwrite(fopen("../file.txt", "w"), "0|0");
-
-} elseif(isset($_GET['getlatest'])){
-
-
-  mysql_connect("localhost", DB_USER, DB_PASS) or die(mysql_error());
-  mysql_select_db(DB_NAME) or die(mysql_error());
-  $q = mysql_query('SELECT * from texts ORDER BY timestamp DESC LIMIT 1')/*  or die(mysql_error()); */
-  /* if(!$q) die('{"room":"0", "recent":"false"}') */;
-  $row = mysql_fetch_assoc($q);
+if(isset($_GET['getlatest'])){
+  header("Content-Type: application/json"); /* Lets JSON this up! */
+  $q = query("SELECT * FROM texts ORDER BY timestamp DESC LIMIT 1");
+  $row = $q->fetch_assoc();
   $recent = "false";
-  header("Content-Type: application/json");
   if((time() - $row['timestamp']) < 3600) $recent = "true";
   echo "{\"room\":\"{$row['text']}\", \"recent\":\"$recent\", \"time\":\"{$row['timestamp']}\"}";
-
-
-
-
-
-
-} else {
-  $file = file_get_contents("../file.txt");
-  $array = explode("|", $file);
-  /* $array[0] is timestamp, $array[1] is room */
-  $room = $array[1];
-  $time = intval($array[0]);
-  $diff = time() - $time;
-  $recent = "false";
-  if($diff < 3600) $recent = "true";
-  header("Content-Type: application/json");
-  echo "{\"room\":\"$room\", \"recent\":\"$recent\"}";
 }
-
 ?>
